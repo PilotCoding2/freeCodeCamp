0817400174001ea41c75751d91234a2f13b533bf
Leon Koller <kevin.koller.p@gmail.com>
2026-05-26 18:37:18 -0600

    Finished another JavaScript project

--- responsive_web/JS-Projects/real-time/script.js
+++ responsive_web/JS-Projects/real-time/script.js
@@ -1,21 +1,30 @@
 const textArea = document.getElementById("text-input");
 const charCounter = document.getElementById("char-count");
+const charNumber = document.getElementById("char-number");
 
 const updateCharCount = () => {
     let currCount = Number(textArea.value.length);
-    if(currCount <= 50){
-        charCounter.textContent = `Character Count: ${currCount}/50`
-    } 
-    console.log(currCount)
+    if(currCount < 51){
+       charNumber.textContent = currCount;
+       charNumber.style.color = '#22D4A0';
+       charCounter.style.color = '#4A8FE7'
+
+        
+    } else {
+        textArea.value = textArea.value.slice(0, 50);
+        charCounter.style.color = '#E8445A';
+        charNumber.style.color = '#E8445A';
+    }
+    
 };
 
 textArea.addEventListener("input", () => {
     updateCharCount();
 });
 
-textArea.addEventListener('keydown', (e) => {
+/*textArea.addEventListener('keydown', (e) => {
     const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'];
     if(!allowedKeys.includes(e.key) && textArea.value.length === 50){
         e.preventDefault();
     }
-});
+});*/
