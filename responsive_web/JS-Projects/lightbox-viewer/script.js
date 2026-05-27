b4d01e14d1f02d0dd023bc8eae75aeec8048c206
Leon Koller <kevin.koller.p@gmail.com>
2026-05-26 22:16:02 -0600

    New project added

---
+++ responsive_web/JS-Projects/lightbox-viewer/script.js
@@ -0,0 +1,27 @@
+const galleryImgs = document.querySelectorAll('.gallery-item');
+const galleryBigDisp = document.querySelector('.lightbox');
+const closeBtn = document.querySelector('#close-btn');
+const bigImg = document.querySelector('#lightbox-image')
+
+const displayImg = (img) => {
+    galleryBigDisp.style.display = 'flex';
+    bigImg.src = img.src;
+}
+
+const closeDisplayImg = () => {
+    galleryBigDisp.style.display = 'none';
+    galleryImgs.forEach(img => {
+        img.style.display = 'block';
+    });
+    
+}
+
+galleryImgs.forEach(img => {
+    img.addEventListener("click", () => {
+        displayImg(img);
+    })
+});
+
+closeBtn.addEventListener("click", () => {
+    closeDisplayImg();
+})
