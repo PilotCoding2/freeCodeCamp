cb2ffb683ac2a55e0d3ea3028edd89164c73ca04
Leon Koller <kevin.koller.p@gmail.com>
2026-06-17 21:54:41 -0600

    Added the RegEx Sandbox

---
+++ responsive_web/JS-Projects/regex-sandbox/script.js
@@ -0,0 +1,33 @@
+const regexPattern = document.getElementById('pattern');
+const stringToTest = document.getElementById('test-string');
+const testButton = document.getElementById('test-btn');
+const testResult = document.getElementById('result');
+
+const caseInsensitiveFlag = document.getElementById('i');
+const globalFlag = document.getElementById('g');
+
+const getFlags = () => {
+    if(caseInsensitiveFlag.checked && globalFlag.checked){
+        return 'ig';
+    } else if (caseInsensitiveFlag.checked && !globalFlag.checked){
+        return 'i';
+    } else if (!caseInsensitiveFlag.checked && globalFlag.checked){
+        return 'g';
+    } else {
+        return '';
+    }
+}
+
+const getMatches = () => {
+    const flags = getFlags();
+    const regex = new RegExp(regexPattern.value, flags);
+    let matches = stringToTest.innerText.match(regex);
+    if(matches){
+        testResult.innerText = matches.join(', ');
+        stringToTest.innerHTML = stringToTest.innerHTML.replace(regex, (content) => `<span class='highlight'>${content}</span>`);
+    } else {
+        testResult.innerText = 'no match';
+    }
+}
+
+testButton.addEventListener('click', getMatches);
